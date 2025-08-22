62e550a2a2a191a2166dd1d541d9cb507612ac8a
magocoro <magocoro@tecez.net>
2025-08-22 13:12:53 +0900

    react init

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: '/my-pages/',
->>>>>>> 5c87916 (react init)
 })
